ad071d54d99e02653a638a4fb367e14273876445
IagoGOOl <iago.gool@gmail.com>
2022-10-26 19:09:53 -0300

    Adicionando Avitidades de POO

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,11 @@
+
+
+class CBBAtividadePratica2 {
+   public static void main(String[] args) {
+      int Contador = 0;
+      while(Contador <= 100){
+         System.out.println(Contador);
+         Contador++;
+      }
+   }
+}
